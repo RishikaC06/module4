ab1414e38ed65682049ea1a0c06da1792cf12232
RishikaC06 <100022132+RishikaC06@users.noreply.github.com>
2022-03-16 17:53:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+var Name=new Array();
+Name[0]="Yaakov"
+Name[1]="John"
+Name[2]="Jen"
+Name[3]="Jason"
+Name[4]="Paul"
+Name[5]="Frank"
+Name[6]="Larry"
+Name[7]="Paula"
+Name[8]="Laura"
+Name[9]="Jim"
+
+for(var i = 0; i < Name.length; i++){
+    if(Name[i].charAt(0) === 'J' || Name[i].charAt(0) ==='j'){
+        console.log("Good Bye "+ Name[i])
+    }
+    else{
+        console.log("Hello "+ Name[i])
+    }    
+}
